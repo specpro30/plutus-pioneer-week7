2d1f0990589af1976bbf7520027a4db43e13be01
specpro30 <specpro30@gmail.com>
2021-05-21 17:03:45 +0800

    completed draft commentary for Test.hs

--- code/week07/src/Week07/Test.hs
+++ code/week07/src/Week07/Test.hs
@@ -12,7 +12,7 @@
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
 
-module Week07.Test where
+module Week07.Test where                                    -- Testing of Alice and Bob game using emulator trace Monad 
 
 import           Control.Monad              hiding (fmap)
 import           Control.Monad.Freer.Extras as Extras
@@ -27,19 +27,19 @@ import           Wallet.Emulator.Wallet
 
 import           Week07.EvenOdd
 
-test :: IO ()
+test :: IO ()                                                         -- in test function all four cominations of GameChoice are run in sequence 
 test = do
     test' Zero Zero
     test' Zero One
     test' One Zero
     test' One One
 
-test' :: GameChoice -> GameChoice -> IO ()
+test' :: GameChoice -> GameChoice -> IO ()                            -- parameterized by two GameChoice one for player one and the other for player two 
 test' c1 c2 = runEmulatorTraceIO' def emCfg $ myTrace c1 c2
   where
-    emCfg :: EmulatorConfig
+    emCfg :: EmulatorConfig                                           -- specify the inital distribution of funds for wallets 
     emCfg = EmulatorConfig $ Left $ Map.fromList
-        [ (Wallet 1, v <> assetClassValue (AssetClass (gameTokenCurrency, gameTokenName)) 1)
+        [ (Wallet 1, v <> assetClassValue (AssetClass (gameTokenCurrency, gameTokenName)) 1)  -- provide wallet 1 with the NFT using the emulatorConfig to mint for testing purposes only  
         , (Wallet 2, v)
         ]
 
@@ -47,28 +47,29 @@ test' c1 c2 = runEmulatorTraceIO' def emCfg $ myTrace c1 c2
     v = Ada.lovelaceValueOf 1000_000_000
 
 gameTokenCurrency :: CurrencySymbol
-gameTokenCurrency = "ff"
+gameTokenCurrency = "ff"                                              -- make up currency symbol and. In real production code we would need to really mint the NFT using the forgeContract from Currency use case
+                                                                      -- or using our own contract from the minting policy lecture
 
 gameTokenName :: TokenName
-gameTokenName = "STATE TOKEN"
+gameTokenName = "STATE TOKEN"                                         -- some arbitrary token name 
 
 myTrace :: GameChoice -> GameChoice -> EmulatorTrace ()
-myTrace c1 c2 = do
+myTrace c1 c2 = do                                                    -- give myTrace two parameters c1 and c2 (choice one and choice two) 
     Extras.logInfo $ "first move: " ++ show c1 ++ ", second move: " ++ show c2
 
-    h1 <- activateContractWallet (Wallet 1) endpoints
-    h2 <- activateContractWallet (Wallet 2) endpoints
+    h1 <- activateContractWallet (Wallet 1) endpoints                 -- start instance for endpoints contract for wallet one 
+    h2 <- activateContractWallet (Wallet 2) endpoints                 -- start instance for endpoints contract for wallet two 
 
-    let pkh1 = pubKeyHash $ walletPubKey $ Wallet 1
+    let pkh1 = pubKeyHash $ walletPubKey $ Wallet 1                   -- look up the pubKeyHash of two wallets 
         pkh2 = pubKeyHash $ walletPubKey $ Wallet 2
 
-        fp = FirstParams
+        fp = FirstParams                                              -- define the parameters for first and second players (first player is wallet 1, second player is wallet 2)
                 { fpSecond         = pkh2
-                , fpStake          = 5000000
-                , fpPlayDeadline   = 5
-                , fpRevealDeadline = 10
-                , fpNonce          = "SECRETNONCE"
-                , fpCurrency       = gameTokenCurrency
+                , fpStake          = 5000000                          -- stake is 5 ADA
+                , fpPlayDeadline   = 5                                -- playdeadline is 5 slots
+                , fpRevealDeadline = 10                               -- revealdeadline is 10 slots 
+                , fpNonce          = "SECRETNONCE"                    -- in reality this should be a random string but for testing we use "SECRETNONCE" 
+                , fpCurrency       = gameTokenCurrency                -- tokens forged using emulatorConfig as the NFT 
                 , fpTokenName      = gameTokenName
                 , fpChoice         = c1
                 }
@@ -82,10 +83,10 @@ myTrace c1 c2 = do
                 , spChoice         = c2
                 }
 
-    callEndpoint @"first" h1 fp
+    callEndpoint @"first" h1 fp                                       -- call the first endpoint on wallet 1 with the fp parameters 
 
-    void $ Emulator.waitNSlots 3
+    void $ Emulator.waitNSlots 3                                      -- wait 3 slots 
 
-    callEndpoint @"second" h2 sp
+    callEndpoint @"second" h2 sp                                      -- then call the second endpoint on wallet 2 with the sp parameters 
 
-    void $ Emulator.waitNSlots 10
+    void $ Emulator.waitNSlots 10                                     -- then wait 10 slots 
